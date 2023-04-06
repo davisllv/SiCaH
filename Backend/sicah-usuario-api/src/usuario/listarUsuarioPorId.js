061530e1c32635e502f92408b21d6effe9a5385f
Vinicius da Cruz Rodrigues Paulo <viniciusdacruzrodrigues@gmail.com>
2023-04-06 01:04:26 -0300

    ajustes

--- Backend/sicah-usuario-api/src/usuario/listarUsuarioPorId.js
+++ Backend/sicah-usuario-api/src/usuario/listarUsuarioPorId.js
@@ -3,10 +3,14 @@
 const { queryDb } = require('../lib/dbSql')
 
 module.exports = async (req, res, next) => {
- try {
-    let usuarios = await queryDb('select',`SELECT id FROM banco01.dbo.usuario WHERE id = ${req.params.id}`);
-    return res.status(200).json(usuarios)
- } catch (e) {
-    return res.status(400).json({ erro: e})
- }
+   try {
+      const query = 'SELECT U.*, E.rua, E.numero, E.complemento, E.bairro, E.cidade, E.estado, E.cep\n' +
+         'FROM banco01.dbo.usuario U\n' +
+         'LEFT JOIN banco01.dbo.endereco E ON E.id = U.id_endereco\n' +
+         `WHERE U.id = ${req.params.id}`
+      let usuario = await queryDb('select', query);
+      return res.status(200).json({ ...usuario[0], senha: '' })
+   } catch (e) {
+      return res.status(400).json({ erro: e })
+   }
 }
--- Backend/sicah-usuario-api/src/usuario/listarUsuarios.js
+++ Backend/sicah-usuario-api/src/usuario/listarUsuarios.js
@@ -3,10 +3,10 @@
 const { queryDb } = require('../lib/dbSql')
 
 module.exports = async (req, res, next) => {
-try {
-    let usuarios = await queryDb('select','SELECT id, Nome, NomeUsuario, Senha, cpf, DataNasc, Sexo, Email, Telefone, id_Endereco FROM banco01.dbo.usuario');
+  try {
+    let usuarios = await queryDb('select', 'SELECT id, nome, email, permite_foto FROM banco01.dbo.usuario');
     return res.status(200).json(usuarios)
- } catch (err) {
-    return res.status(500).json({ erro: err})
- }
+  } catch (err) {
+    return res.status(500).json({ erro: err })
+  }
 }
