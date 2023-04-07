4b367414007d6a506a838bfa41f06a541d2152e6
Vinicius da Cruz Rodrigues Paulo <viniciusdacruzrodrigues@gmail.com>
2023-04-07 20:45:21 -0300

    backend: adicionados parametros para paginacao

--- Backend/sicah-usuario-api/src/usuario/listarUsuarios.js
+++ Backend/sicah-usuario-api/src/usuario/listarUsuarios.js
@@ -4,8 +4,13 @@ const { queryDb } = require('../lib/dbSql')
 
 module.exports = async (req, res, next) => {
   try {
-    let usuarios = await queryDb('select', 'SELECT id, nome, email, permite_foto FROM banco01.dbo.usuario');
-    return res.status(200).json(usuarios)
+    const { take, skip } = req.query;
+    const query = 'SELECT id, nome, email, permite_foto, COUNT(*) OVER() as Total FROM banco01.dbo.usuario ' +
+      'ORDER BY nome ' +
+      `OFFSET ${skip} ROWS FETCH NEXT ${take} ROWS ONLY`;
+    let usuarios = await queryDb('select', query);
+    const total = usuarios[0].Total;
+    return res.status(200).json({ users: usuarios, total })
   } catch (err) {
     return res.status(500).json({ erro: err })
   }
