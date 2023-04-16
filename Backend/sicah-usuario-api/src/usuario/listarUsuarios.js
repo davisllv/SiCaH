a3dab364def90732c33d96efe79c38897c6417ca
Vinicius da Cruz Rodrigues Paulo <viniciusdacruzrodrigues@gmail.com>
2023-04-16 15:11:58 -0300

    ajuste query

--- Backend/sicah-usuario-api/src/usuario/listarUsuarios.js
+++ Backend/sicah-usuario-api/src/usuario/listarUsuarios.js
@@ -4,10 +4,15 @@ const { queryDb } = require('../lib/dbSql')
 
 module.exports = async (req, res, next) => {
   try {
-    const { take, skip } = req.query;
-    const query = 'SELECT Usuario.id, Usuario.nome, email, permite_foto, Empresa.nome AS nome_empresa, COUNT(*) OVER() as Total FROM banco01.dbo.usuario Usuario ' +
-      'LEFT JOIN banco01.dbo.empresa Empresa ON Empresa.id = Usuario.id_empresa ' +
-      'ORDER BY nome ' +
+    const { take, skip, companyId } = req.query;
+    let query = 'SELECT Usuario.id, Usuario.nome, email, permite_foto, Empresa.nome AS nome_empresa, COUNT(*) OVER() as Total FROM banco01.dbo.usuario Usuario ' +
+      'LEFT JOIN banco01.dbo.empresa Empresa ON Empresa.id = Usuario.id_empresa ';
+
+    if (companyId !== '0' && companyId !== 'undefined') {
+      query += 'WHERE Usuario.id_empresa = ' + companyId;
+    }
+
+    query += 'ORDER BY nome ' +
       `OFFSET ${skip} ROWS FETCH NEXT ${take} ROWS ONLY`;
     let usuarios = await queryDb('select', query);
     if (usuarios.mensagem) {
