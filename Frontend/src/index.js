ab4415d5c52e768761f6f9c1ff58c907c15add13
Vinicius da Cruz Rodrigues Paulo <viniciusdacruzrodrigues@gmail.com>
2023-04-16 14:39:51 -0300

    adicionado localizacao na aplicacao

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/locale/pt_BR';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <ConfigProvider locale={ptBR}>
+      <App />
+    </ConfigProvider>
   </BrowserRouter>
 );
 
