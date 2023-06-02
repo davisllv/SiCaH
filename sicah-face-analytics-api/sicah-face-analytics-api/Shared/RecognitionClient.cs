9b4015e7a52b6d8377cdfdf136948d1fdd6adc79
Vinicius da Cruz Rodrigues Paulo <viniciusdacruzrodrigues@gmail.com>
2023-06-01 23:28:45 -0300

    adicionado api para deteccao facial

---
+++ sicah-face-analytics-api/sicah-face-analytics-api/Shared/RecognitionClient.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Rekognition;
+
+namespace sicah_face_analytics_api.Shared
+{
+    public class RecognitionClient
+    {
+        private readonly string _awsAccessKeyId;
+        private readonly string _awsSecretAccessKey;
+        public readonly AmazonRekognitionClient? client;
+
+        public RecognitionClient()
+        {
+            _awsAccessKeyId = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID") ?? "";
+            _awsSecretAccessKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY") ?? "";
+            if (!string.IsNullOrEmpty(_awsAccessKeyId) && !string.IsNullOrEmpty(_awsSecretAccessKey))
+            {
+                client = new AmazonRekognitionClient(_awsAccessKeyId, _awsSecretAccessKey, Amazon.RegionEndpoint.USEast2);
+            }
+        }
+    }
+}
