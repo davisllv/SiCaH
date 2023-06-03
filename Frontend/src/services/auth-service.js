a74a6427bbf3702a76c73c25cb5d56a35e9971a9
Vinicius da Cruz Rodrigues Paulo <viniciusdacruzrodrigues@gmail.com>
2023-06-03 15:56:58 -0300

    atualizados links das apis

--- Frontend/src/services/auth-service.js
+++ Frontend/src/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // TODO: adicionar url da API
-const API_URL = "http://localhost:3000";
+const API_URL = "https://apisicah.azurewebsites.net";
 
 class AuthService {
   login(email, password) {
