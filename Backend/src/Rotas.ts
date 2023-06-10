0023fd3b0fd3b34f414d58dfae12263a692a3f95
Igor Zysko <igor.zysko@cwi.com.br>
2023-06-09 21:50:43 -0300

    Add crud equipamento

--- Backend/src/Rotas.ts
+++ Backend/src/Rotas.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import UsuarioRotas from "./User/Routes/UsuarioRotas";
 import EmpresaRotas from "./User/Routes/EmpresaRotas";
+import EquipamentoRotas from "./User/Routes/EquipamentoRotas";
+
 
 const Rota = Router();
 Rota.use("/usuario", UsuarioRotas);
-
+Rota.use("/equipamento", EquipamentoRotas);
 Rota.use("/empresa", EmpresaRotas);
 
 export default Rota;
